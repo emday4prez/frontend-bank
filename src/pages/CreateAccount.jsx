514f634ee677d9b85a8adb444eb5ce58e0561bd5
Emerson <emerson.day@gmail.com>
2022-09-01 16:18:22 -0400

    added form validation

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,33 +1,77 @@
-import { useContext, useState } from 'react';
+import { useContext, useState} from 'react';
 import { UserContext } from '../context';
-
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 function CreateAccount() {
     const context = useContext(UserContext);
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [hasInput, setHasInput] = useState(false);
+    const [accountAdded, setAccountAdded] = useState(false);
+    const [displaySuccessAlert, setDisplaySuccessAlert] = useState(false)
 
     const handleNameChange = (e) => {
-      setName(e.target.value)
-    }
+        setName(e.target.value);
+        if (!hasInput) setHasInput(true)
+    };
     const handleEmailChange = (e) => {
-      setEmail(e.target.value)
-    }
+        setEmail(e.target.value);
+        if (!hasInput) setHasInput(true);
+    };
     const handlePasswordChange = (e) => {
-      setPassword(e.target.value)
+        setPassword(e.target.value);
+        if (!hasInput) setHasInput(true);
+    };
+    const resetForm = () => {
+      setName('')
+      setEmail('')
+      setPassword('')
+      setHasInput(false)
+      setAccountAdded(true)
     }
 
+    
+
     const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log(`name: ${name}, email: ${email}`)
-      console.log(`password: ${password}`)
-    }
+        e.preventDefault();
+        if (name.trim().length < 1) {
+            alert('name is required.');
+            return;
+        } else if (email.trim().length < 1) {
+            alert('E-mail is required.');
+            return;
+        } else if (password.trim().length < 8) {
+            alert('Password must be at least 8 characters.');
+            return;
+        } else {
+            const newUser = {
+                name: name,
+                email: email,
+                password: password,
+                balance: 0,
+            };
+            context.users = [...context.users, newUser];
+            console.log(context.users);
+            resetForm();
+            setDisplaySuccessAlert(true)
+            setTimeout(() => {
+              setDisplaySuccessAlert(false)
+            }, 5000)
+        }
+    };
+
+    
+
     return (
         <div className="page">
+            {displaySuccessAlert && (
+                <Alert variant={'success'}>
+                    Successfully created user!
+                </Alert>
+            )}
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
@@ -35,6 +79,7 @@ function CreateAccount() {
                         type="text"
                         placeholder="Enter name"
                         onChange={handleNameChange}
+                        value={name}
                     />
                     <Form.Text className="text-muted">
                         Please enter your first name.
@@ -46,6 +91,7 @@ function CreateAccount() {
                         type="email"
                         placeholder="Enter email"
                         onChange={handleEmailChange}
+                        value={email}
                     />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
@@ -58,14 +104,20 @@ function CreateAccount() {
                         type="password"
                         placeholder="Password"
                         onChange={handlePasswordChange}
+                        value={password}
                     />
                     <Form.Text className="text-muted">
                         This is a fake bank, passwords are not securely stored.
                     </Form.Text>
                 </Form.Group>
 
-                <Button variant="primary" onClick={handleSubmit} type="submit">
-                    Create Account
+                <Button
+                    variant="primary"
+                    onClick={handleSubmit}
+                    className={hasInput ? '' : 'disabled'}
+                    type="submit"
+                >
+                    {accountAdded ? 'Add Another Account' : 'Create Account'}
                 </Button>
             </Form>
         </div>
