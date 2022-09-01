86e01ae59bef134551ccf9750c66689b4b9e5e6a
Emerson <emerson.day@gmail.com>
2022-09-01 10:22:03 -0400

    added form

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,12 +1,44 @@
-import {useContext} from 'react'
-import {UserContext} from '../context'
- 
-function CreateAccount() {
- const context = useContext(UserContext)
+import { useContext } from 'react';
+import { UserContext } from '../context';
 
-  return (
-    <div>{JSON.stringify(context)}</div>
-  )
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+function CreateAccount() {
+    const context = useContext(UserContext);
+
+    return (
+        <div className="page">
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" />
+                    <Form.Text className="text-muted">
+                        Please enter your first name.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Text className="text-muted">
+                        This is a fake bank, passwords are not securely stored.
+                    </Form.Text>
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Create Account
+                </Button>
+            </Form>
+        </div>
+    );
 }
 
-export default CreateAccount
+export default CreateAccount;
