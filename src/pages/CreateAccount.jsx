941d01593f599050c88ced438e6aaed18bf87d54
Emerson <emerson.day@gmail.com>
2022-09-01 13:54:12 -0400

    added useState for forms

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../context';
 
 import Button from 'react-bootstrap/Button';
@@ -6,20 +6,47 @@ import Form from 'react-bootstrap/Form';
 
 function CreateAccount() {
     const context = useContext(UserContext);
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
+
+    const handleNameChange = (e) => {
+      setName(e.target.value)
+    }
+    const handleEmailChange = (e) => {
+      setEmail(e.target.value)
+    }
+    const handlePasswordChange = (e) => {
+      setPassword(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(`name: ${name}, email: ${email}`)
+      console.log(`password: ${password}`)
+    }
     return (
         <div className="page">
             <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="Enter name" />
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter name"
+                        onChange={handleNameChange}
+                    />
                     <Form.Text className="text-muted">
                         Please enter your first name.
                     </Form.Text>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        onChange={handleEmailChange}
+                    />
                     <Form.Text className="text-muted">
                         We'll never share your email with anyone else.
                     </Form.Text>
@@ -27,13 +54,17 @@ function CreateAccount() {
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control
+                        type="password"
+                        placeholder="Password"
+                        onChange={handlePasswordChange}
+                    />
                     <Form.Text className="text-muted">
                         This is a fake bank, passwords are not securely stored.
                     </Form.Text>
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
+                <Button variant="primary" onClick={handleSubmit} type="submit">
                     Create Account
                 </Button>
             </Form>
