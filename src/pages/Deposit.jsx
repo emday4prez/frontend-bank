a9e434f6666eddac00ebfa6a6083e6b89bf2e450
Emerson <emerson.day@gmail.com>
2022-09-04 12:50:26 -0400

    started deposit page

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -1,9 +1,25 @@
-
-
+import { useContext, useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { UserContext } from '../context';
 function Deposit() {
-  return (
-    <div>Deposit</div>
-  )
+
+  const context = useContext(UserContext)
+    return (
+        <div className="page">
+            <Card>
+                <Card.Header>Make A Deposit</Card.Header>
+                <Card.Body>
+                    <Card.Title>{`Account Balance: $${context.users[0].balance}`}</Card.Title>
+                    <Card.Text>
+                        With supporting text below as a natural lead-in to
+                        additional content.
+                    </Card.Text>
+                    <Button variant="primary">Deposit</Button>
+                </Card.Body>
+            </Card>
+        </div>
+    );
 }
 
-export default Deposit
+export default Deposit;
