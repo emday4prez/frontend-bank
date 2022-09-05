2453058e2375be020f1a4e745f01b7d1e01bed33
Emerson <emerson.day@gmail.com>
2022-09-05 13:37:13 -0400

    added state for deposit

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -1,21 +1,51 @@
 import { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 import { UserContext } from '../context';
-function Deposit() {
 
-  const context = useContext(UserContext)
+function Deposit() {
+    const context = useContext(UserContext);
+    const [depositAmount, setDepositAmount] = useState(0);
+    const submitDeposit = (e) => {
+        e.preventDefault();
+        let balance = context.users[0].balance;
+        balance = balance + +depositAmount;
+
+        context.users[0].balance = +balance;
+        console.log(context.users[0].balance);
+    };
+
     return (
         <div className="page">
             <Card>
                 <Card.Header>Make A Deposit</Card.Header>
                 <Card.Body>
                     <Card.Title>{`Account Balance: $${context.users[0].balance}`}</Card.Title>
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to
-                        additional content.
-                    </Card.Text>
-                    <Button variant="primary">Deposit</Button>
+                    <Form style={{ minWidth: '300px' }}>
+                        <Form.Group className="mb-3" controlId="formBasicName">
+                            <Form.Label>Deposit</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="deposit amount"
+                                value={depositAmount}
+                                onChange={(e) =>
+                                    setDepositAmount(e.target.value)
+                                }
+                            />
+                            <Form.Text className="text-muted">
+                                Enter a number amount to deposit.
+                            </Form.Text>
+                        </Form.Group>
+                    </Form>
+
+                    <Button
+                        variant="primary"
+                        className={depositAmount !== 0 ? '' : 'disabled'}
+                        onClick={submitDeposit}
+                    >
+                        Deposit
+                    </Button>
                 </Card.Body>
             </Card>
         </div>
