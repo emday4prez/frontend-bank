f704a6abf6c4fcbfe4ea4d5ce8f928364d46d4b6
Emerson <emerson.day@gmail.com>
2022-09-05 17:07:54 -0400

    added deposit page functionality

--- src/pages/Deposit.jsx
+++ src/pages/Deposit.jsx
@@ -2,22 +2,60 @@ import { useContext, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 import { UserContext } from '../context';
 
 function Deposit() {
     const context = useContext(UserContext);
     const [depositAmount, setDepositAmount] = useState(0);
+    const [negativeError, setNegativeError] = useState(false);
+    const [nanError, setNanError] = useState(false);
+    const [successful, setSuccessful] = useState(false);
+
+    const onDepositChange = (e) => {
+        console.log(+e.target.value);
+        setDepositAmount(+e.target.value);
+    };
     const submitDeposit = (e) => {
         e.preventDefault();
-        let balance = context.users[0].balance;
-        balance = balance + +depositAmount;
+        if (depositAmount < 1) {
+            setNegativeError(true);
+            setTimeout(() => {
+                setNegativeError(false);
+            }, 3000);
+        } else if (typeof depositAmount !== 'number') {
+            setNanError(true);
+            setTimeout(() => {
+                setNanError(false);
+            }, 3000);
+        } else {
+            let balance = context.users[0].balance;
+            balance = balance + +depositAmount;
 
-        context.users[0].balance = +balance;
-        console.log(context.users[0].balance);
+            context.users[0].balance = +balance;
+            console.log(context.users[0].balance);
+            setSuccessful(true);
+            setTimeout(() => {
+                setSuccessful(false);
+            }, 3000);
+        }
     };
 
     return (
         <div className="page">
+            <div style={{ minHeight: '75px' }}>
+                {successful && (
+                    <Alert variant={'success'}>Deposit Successful!</Alert>
+                )}
+                {negativeError && (
+                    <Alert variant={'danger'}>
+                        deposit amount must be more than 0!
+                    </Alert>
+                )}
+                {nanError && (
+                    <Alert variant={'danger'}>Input must be a number!</Alert>
+                )}
+            </div>
             <Card>
                 <Card.Header>Make A Deposit</Card.Header>
                 <Card.Body>
@@ -28,10 +66,8 @@ function Deposit() {
                             <Form.Control
                                 type="number"
                                 placeholder="deposit amount"
+                                onChange={onDepositChange}
                                 value={depositAmount}
-                                onChange={(e) =>
-                                    setDepositAmount(e.target.value)
-                                }
                             />
                             <Form.Text className="text-muted">
                                 Enter a number amount to deposit.
